1aca4eaae8bf01c96b80e190403c175e30c463fb
Alan Lima <alan.lima123@hotmail.com>
2020-09-11 21:19:01 -0300

    conectando ao mongodb com repository e service

---
+++ src/main/java/com/alanlima/springmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.alanlima.springmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alanlima.springmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
