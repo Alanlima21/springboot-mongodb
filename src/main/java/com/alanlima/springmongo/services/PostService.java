be896715442868d9aa0e3a8111d69fcd0e715d22
Alan Lima <alan.lima123@hotmail.com>
2020-09-22 21:35:49 -0300

    obtendo post por id

---
+++ src/main/java/com/alanlima/springmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.alanlima.springmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alanlima.springmongo.domain.Post;
+import com.alanlima.springmongo.repository.PostRepository;
+import com.alanlima.springmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional <Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));	
+	}
+	
+	
+}
