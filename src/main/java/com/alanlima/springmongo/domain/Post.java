d1db21c4c94b088ce4887cb5407f220b28cdb5d8
Alan Lima <alan.lima123@hotmail.com>
2020-09-22 22:05:15 -0300

    acrescentando comentários aos pots

--- src/main/java/com/alanlima/springmongo/domain/Post.java
+++ src/main/java/com/alanlima/springmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.alanlima.springmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.alanlima.springmongo.dto.AuthorDTO;
+import com.alanlima.springmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -17,6 +20,9 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	private AuthorDTO author;
 	
 	public Post() {
@@ -72,7 +78,10 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
-
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
